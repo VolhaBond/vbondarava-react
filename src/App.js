c5c708c4955807057167ae8794d848bac45db62b
Volha Bondarava <vbondarava@gomel.iba.by>
2022-06-15 17:38:14 +0400

    React - Task 7-1 - move SignIn to the right top corner and hide if user is loggedin

--- src/App.js
+++ src/App.js
@@ -8,13 +8,14 @@ import './index.css';
 
 const App = () => {
   const userCtx = useContext(UserCtx);
-  
+
   return (
     <div>
       <NavLink className={(navData) => navData.isActive ? 'active' : ''} to='home' >Home</NavLink>
-      <NavLink className={(navData) => navData.isActive ? 'active' : ''} to='signin' >Sign In</NavLink>
-      
-{userCtx.userName} 
+      <div className="icon_right">
+        {!userCtx.userName && <NavLink className={(navData) => navData.isActive ? 'active' : ''} to='signin' >Sign In</NavLink>}
+        {userCtx.userName}
+      </div>
       <Routes>
         <Route path="/" element={<Navigate to='home' />} />
         <Route path="/home" element={<Cards />} />
