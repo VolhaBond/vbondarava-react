86a790ac786f91ff418d6a28fe5434e869a26cbd
Volha Bondarava <vbondarava@gomel.iba.by>
2022-06-23 17:22:14 +0400

    React - Task 8-1 - combine states

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,5 +1,5 @@
-import {useContext, useState} from 'react';
-import {useNavigate} from 'react-router-dom';
+import { useContext, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import UserCtx from '../context/user-context';
 import EnhancedInput from '../Helpers/EnhancedInput';
 import validations from '../Helpers/Validations';
@@ -9,29 +9,30 @@ const SignIn = props => {
     const userCtx = useContext(UserCtx);
     const navigate = useNavigate();
     const [userData, setUserData] = useState({
-        username: '',
-        password: ''
-    });
-
-    const [fieldHasError, setFieldHasError] = useState({
-        username: false,
-        password: false
+        username: {
+            value: '',
+            hasError: false
+        },
+        password: {
+            value: '',
+            hasError: false
+        }
     });
 
     const emailValidations = [validations.required, validations.emailPattern];
-    const passwordValidations = [validations.required, validations.minLength,validations.passwordPattern];
+    const passwordValidations = [validations.required, validations.minLength, validations.passwordPattern];
 
     const inputsData = [
         {
-            type: "text", 
-            id: "username", 
+            type: "text",
+            id: "username",
             label: "User name",
             rules: emailValidations,
             autocomplete: "off"
         },
         {
-            type: "password", 
-            id: "password", 
+            type: "password",
+            id: "password",
             label: "Password",
             rules: passwordValidations,
             autocomplete: "on"
@@ -39,47 +40,48 @@ const SignIn = props => {
     ]
 
     const signOnHandler = event => {
-        event.preventDefault(); 
+        event.preventDefault();
         navigate('/home');
-        userCtx.logIn(userData.username);
+        userCtx.logIn(userData.username.value);
     }
 
     const handleObjChange = (field, value, errorText) => {
         setUserData({
             ...userData,
-            [field]: value
-        });
-        
-        setFieldHasError({
-            ...fieldHasError,
-            [field]: errorText ? false : true
-        });
+            [field]: {
+                value: value,
+                hasError: errorText ? false : true
+            }
+        })
+
     }
 
-    const formIsValid = fieldHasError.username && fieldHasError.password;
+    const formIsValid = Object.keys(userData).reduce(function (previous, field) {
+        return previous && userData[field].hasError;
+    }, true); 
 
-    return (
-        <div className='input' >
-            <section className='modal-main'>
-                <form className='tab' onSubmit={signOnHandler}>
-                    <h1 className="header">
-                        Sign in here.
+        return(
+            <div className='input' >
+                <section className='modal-main'>
+                    <form className='tab' onSubmit={signOnHandler}>
+                        <h1 className="header">
+                            Sign in here.
                     </h1>
 
-                    {inputsData.map(input => 
-                        <EnhancedInput 
-                            key={input.id}
-                            {...input}
-                            handleObjChange={handleObjChange} 
-                            validate={validations.composeValidators(...input.rules)}
-                        />
-                    )}
-                    
-                    <button type='submit' disabled={!formIsValid} className="modal_form"> Login</button>
-                </form>
-            </section>
-        </div>
-    );
+                        {inputsData.map(input =>
+                            <EnhancedInput
+                                key={input.id}
+                                {...input}
+                                handleObjChange={handleObjChange}
+                                validate={validations.composeValidators(...input.rules)}
+                            />
+                        )}
+
+                        <button type='submit' disabled={!formIsValid} className="modal_form"> Login</button>
+                    </form>
+                </section>
+            </div>
+        );
 }
 
 export default SignIn;
