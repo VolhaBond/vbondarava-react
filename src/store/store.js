f246bb3f759e1be3cc17e2eb38ed18d9e42a2905
Бондарева Ольга Олеговна <VBondarava@gomel.iba.by>
2023-04-12 14:36:55 +0300

    React - Task 10-1

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
+import {createLogger} from 'redux-logger'
+
 import cardReducer from './card';
 import userReducer from './user';
+
+const logger = createLogger({
+    log: 'info',
+})
 
 const store = configureStore({
     reducer: {
         card: cardReducer,
         user: userReducer
-    }
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 export default store;
